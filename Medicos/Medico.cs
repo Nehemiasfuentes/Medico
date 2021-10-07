0d925f398880e2f88a2ce0929b61cb2a0dca4335
Nehemias Fuentes <isaifuentes2017@gmail.com>
2021-10-06 20:47:18 -0600

    Agregar archivos de proyecto.

---
+++ Medicos/Medico.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medicos
+{
+    class Medico
+    {
+
+        private string nombre;
+        private string apellido;
+        private string especialidad;
+        private int telefono;
+        private string direccionClinica;
+
+        public void setnombre(string Nombre)
+        {
+            nombre = Nombre;
+        }
+
+        public string getnombre()
+        {
+            return nombre;
+        }
+
+        public void setapellido(string Apellido)
+        {
+            apellido = Apellido;
+        }
+
+        public string getapellido()
+        {
+            return apellido;
+        }
+
+        public void setespecialidad(string Especialidad)
+        {
+            especialidad = Especialidad;
+        }
+
+        public string getespecialidad()
+        {
+            return especialidad;
+        }
+
+        public void settelefono(int Telefono)
+        {
+            telefono = Telefono;
+        }
+
+        public int gettelefono()
+        {
+            return telefono;
+        }
+
+        public void setdireccion(string DireccionClinica)
+        {
+            direccionClinica = DireccionClinica;
+        }
+
+        public string getdireccion()
+        {
+            return direccionClinica;
+        }
+    }
+}
