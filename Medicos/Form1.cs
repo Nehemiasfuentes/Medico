1197b6d626b3a7b5e0ac1c3178bc8d47a820b851
Nehemias Fuentes <isaifuentes2017@gmail.com>
2021-10-06 21:10:44 -0600

    Diseño de la interfaz del formulario de medico.

--- Medicos/Form1.cs
+++ Medicos/Form1.cs
@@ -16,5 +16,33 @@ namespace Medicos
         {
             InitializeComponent();
         }
+
+   
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Medico medico = new Medico();
+
+            medico.setnombre(textBoxNombre.Text);
+            medico.setapellido(textBoxApellido.Text);
+            medico.setespecialidad(textBoxEspecialidad.Text);
+            medico.settelefono(Convert.ToInt32(textBoxTelefono.Text));
+            medico.setdireccion(textBoxDireccion.Text);
+
+            dataGridViewInfo.Rows.Add(medico.getnombre(), medico.getapellido(), medico.getespecialidad(), medico.gettelefono(), medico.getdireccion());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridViewInfo.Columns.Add("nombre", "Nombre");
+            dataGridViewInfo.Columns.Add("apellido", "Apellido");
+            dataGridViewInfo.Columns.Add("especialidad", "Especialidad");
+            dataGridViewInfo.Columns.Add("telefono", "Telefono");
+            dataGridViewInfo.Columns.Add("direccionClinica", "Direccion Clinica");
+        }
     }
 }
